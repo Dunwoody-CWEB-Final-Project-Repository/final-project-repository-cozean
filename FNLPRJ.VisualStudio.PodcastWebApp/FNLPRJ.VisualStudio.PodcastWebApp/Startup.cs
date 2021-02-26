2dd4c62044aca89eef66ea6ca7d987a768fa0b74
Bryan_Cozean <cozbryj@dunwoody.edu>
2021-02-26 11:35:11 -0600

    Initial Project Commit
    MVC 5 with authentication folder structure.

---
+++ FNLPRJ.VisualStudio.PodcastWebApp/FNLPRJ.VisualStudio.PodcastWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FNLPRJ.VisualStudio.PodcastWebApp.Startup))]
+namespace FNLPRJ.VisualStudio.PodcastWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
